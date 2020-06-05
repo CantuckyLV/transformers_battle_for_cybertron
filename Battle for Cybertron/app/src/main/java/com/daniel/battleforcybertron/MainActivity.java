3958cd40a816b129ab6a1457d479c65013dce655
CantuckyLV <danielgc0794@gmail.com>
2020-06-04 17:24:55 -0700

    Some tests

--- Battle for Cybertron/app/src/main/java/com/daniel/battleforcybertron/MainActivity.java
+++ Battle for Cybertron/app/src/main/java/com/daniel/battleforcybertron/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("holo","holo");
+        Toast.makeText(this, "test lel", Toast.LENGTH_SHORT).show();
     }
 }
